f1fe4cc812999d7d2567d66c4a8dd823a084ad21
Ben Mosher <me@benmosher.com>
2015-08-04 08:37:05 -0400

    added REPL, colors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 "use strict"
 
 const _ = require('lodash')
+const readline = require('readline')
+const colorText = require('colors/safe')
 
 const colors = ['red', 'yellow', 'green', 'blue', 'white']
 
@@ -25,12 +27,18 @@ function deal() {
 
 let hands = [[], [], [], []]
   , discards = []
-  , played = []
+  , played = {}
 
+// deal
 for (let i = 0; i < 4; i++) {
   for (let hand of hands) {
     hand.push(deal())
   }
+}
+
+// set up board
+for (let color of colors) {
+  played[color] = 0
 }
 
 const MAX_CLUES = 8
@@ -39,25 +47,54 @@ let clues = 8
 
 let player = 0
 
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+rl.setPrompt('>')
+
 function printState() {
-  console.log(`you are player ${player}.`)
-  console.log(`there are ${clues} clues and ${fuses} fuses.`)
+  rl.output.write(`you are player ${player}.\n`)
+  rl.output.write(`there are ${clues} clues and ${fuses} fuses.\n`)
 
   hands.forEach(function (hand, playerIndex) {
     if (playerIndex === player) return
     printPlayer(playerIndex, hand)
   })
 
-  console.log(`discards are: ${handString(discards)}`)
-  console.log(`played are: ${handString(played)}`)
+  rl.output.write(`discards are: ${handString(discards)}\n`)
+  rl.output.write(`played are: ${boardString(played)}\n`)
 }
 
 function printPlayer(player, hand) {
-  console.log(`player ${player} has ${handString(hand)}.`)
+  rl.output.write(`player ${player} has ${handString(hand)}.\n`)
 }
 
 function handString(hand) {
-  return hand.map(function (h) { return `${h.color} ${h.number}`}).join(', \t')
+  return hand.map(function (h) { return colorText[h.color](`${h.color} ${h.number}`)}).join(', \t')
 }
 
-printState()
+function boardString(board) {
+  return JSON.stringify(board) // for now
+}
+
+function isPlayable(tile, board) {
+  return board[tile.color] === tile.number - 1
+}
+
+// repl
+
+rl.output.write("let's play.\n")
+rl.prompt()
+
+rl.on('line', function (line) {
+  console.log('entered line')
+  printState()
+  rl.prompt()
+}) 
+rl.on('close', function () { 
+  rl.output.write('goodbye.\n')
+  process.exit(0)
+})
+
